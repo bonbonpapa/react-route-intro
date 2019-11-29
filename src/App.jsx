f0dd1b123a4962cee60576abae3448e36d1e787d
bonbonpapa <dui.zhang@gmail.com>
2019-11-28 20:10:16 -0500

    copy the workshop docs

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
-import React, { Component } from 'react'
-
-class App extends Component {
-    render = () => {
-        return "Hello world!"
-    }
-}
-
-
-export default App
+import React, { Component } from 'react' 
+import { BrowserRouter, Route, Link } from 'react-router-dom' 
+let renderRoot = () => { 
+    return (<div> 
+        Welcome to this page. Click here to view all the items: 
+        <Link to="/allItems">All items</Link> 
+    </div>) 
+} 
+let renderAllItems = () => { 
+    return (<div> 
+        <div><Link to="/item/abc">A hat</Link></div> 
+        <div><Link to="/item/def">A boat</Link></div> 
+    </div>) 
+} 
+let renderItem = routerData => { 
+    if (routerData.match.params.itemId === "abc") { 
+        return (<div>A very nice hat. Only 4 remaining <img src="/hat.jpg" /></div>) 
+    } 
+    if (routerData.match.params.itemId === "def") { 
+        return (<div>A boat. Only 4 remaining <img src="/boat.jpg" /></div>) 
+    } 
+    return (<div> Unknown item </div>) 
+} 
+class App extends Component { 
+    render() { 
+        return (<BrowserRouter><div> 
+            <Route exact={true} path='/' render={renderRoot} /> 
+            <Route exact={true} path='/allItems' render={renderAllItems} /> 
+            <Route exact={true} path='/item/:itemId' render={renderItem} /> 
+        </div></BrowserRouter>) 
+    } 
+} 
+export default App 
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,5 @@
-import ReactDOM from 'react-dom'
-import './main.css'
-import App from './App.jsx'
-import React from 'react'
-
-import reloadMagic from './reload-magic-client.js' // automatic reload
-reloadMagic() // automatic reload
-
-ReactDOM.render(<App />, document.getElementById("root"))
+import ReactDOM from 'react-dom' 
+import './main.css' 
+import App from './App.jsx' 
+import React, { Component } from 'react' 
+ReactDOM.render(<App />, document.getElementById("root")) 

6fbc579306e5412aa42734ec3a4dd9232b6d452f
bonbonpapa <dui.zhang@gmail.com>
2019-11-30 11:37:21 -0500

    data as separate

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
+import {data, sellers} from "./data"
 let renderRoot = () => {
   return (
     <div>
@@ -9,49 +10,7 @@ let renderRoot = () => {
   );
 };
 
-let data = [
-  {
-    name: "hat",
-    remaining: 1,
-    pic: "hat.jpg",
-    identifier: "abc",
-    Description: "A beatiful hat",
-    sellerId: "s001"
-  },
-  {
-    name: "boat",
-    remaining: 10,
-    pic: "boat.jpg",
-    identifier: "def",
-    Description: "A great boat",
-    sellerId: "s002"
-  },
-  {
-    name: "lawnmower",
-    remaining: 5,
-    pic: "lawnmower.jpg",
-    identifier: "lmr",
-    Description: "A useful lawnmower",
-    sellerId: "s003"
-  },
-  {
-    name: "snowshoes",
-    remaining: 5,
-    pic: "snowshoes.jpg",
-    identifier: "sns",
-    Description: "A pair of snowshoes for winter",
-    sellerId: "s004"
-  }
-];
 
-let sellers = [
-    { sellerId: "s001", name: "abc.com", seller_desc: "Dedicated hat manufacturer" },
-    { sellerId: "s002", name: "abc.com", seller_desc: "Dedicated boad manufacturer" },
-    { sellerId: "s003", name: "abc.com", seller_desc: "Dedicated lawnmower manufacturer" },
-    { sellerId: "s004", name: "abc.com", seller_desc: "Dedicated snowshoes manufacturer" },
-
-
-]
 
 class SellerItem extends Component {
     render = () => {
@@ -78,6 +37,7 @@ class ShoppingItem extends Component {
         let sellerpath = "/seller/" + toDisplay.sellerId;
         return (
             <div>
+                <h3>Details</h3>
                 <div>{"Description: " + toDisplay.Description}</div>
                 <div>{"Remaining: " + toDisplay.remaining}</div>
                 <div>{"Seller information: "}<Link to={sellerpath}>{toDisplay.sellerId}</Link></div>
@@ -98,7 +58,8 @@ let renderAllItems = () => {
             let path = "/item/" + e.identifier
             return (
               <div>
-                <Link to={path}>{e.Description}</Link>
+                {/* <Link to={path}>{e.Description}</Link> */}
+                <Link to = {`/item/${e.identifier}`}>{e.Description}</Link>
                 <img src={"/" + e.pic} width = "100px" height = "100px" hspace= "20px"/>
                 </div>
             );
---
+++ src/data.js
@@ -0,0 +1,45 @@
+export const data = [
+    {
+      name: "hat",
+      remaining: 1,
+      pic: "hat.jpg",
+      identifier: "abc",
+      Description: "A beatiful hat",
+      sellerId: "s001"
+    },
+    {
+      name: "boat",
+      remaining: 10,
+      pic: "boat.jpg",
+      identifier: "def",
+      Description: "A great boat",
+      sellerId: "s002"
+    },
+    {
+      name: "lawnmower",
+      remaining: 5,
+      pic: "lawnmower.jpg",
+      identifier: "lmr",
+      Description: "A useful lawnmower",
+      sellerId: "s003"
+    },
+    {
+      name: "snowshoes",
+      remaining: 5,
+      pic: "snowshoes.jpg",
+      identifier: "sns",
+      Description: "A pair of snowshoes for winter",
+      sellerId: "s004"
+    }
+  ];
+  
+  export const sellers = [
+      { sellerId: "s001", name: "abc.com", seller_desc: "Dedicated hat manufacturer" },
+      { sellerId: "s002", name: "abc.com", seller_desc: "Dedicated boad manufacturer" },
+      { sellerId: "s003", name: "abc.com", seller_desc: "Dedicated lawnmower manufacturer" },
+      { sellerId: "s004", name: "abc.com", seller_desc: "Dedicated snowshoes manufacturer" },
+  
+  
+  ]
+
+
